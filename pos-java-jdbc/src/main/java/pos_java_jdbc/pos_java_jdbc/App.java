0c38ad58f14058ede9d556225822077aa98b2dac
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-24 06:50:07 -0300

    20.7 - Criando o projeto com Maven

---
+++ pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/App.java
@@ -0,0 +1,13 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
